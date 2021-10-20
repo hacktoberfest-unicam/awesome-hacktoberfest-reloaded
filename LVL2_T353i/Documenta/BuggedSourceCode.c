6d01e687d86e240ab40720bdae7029cfb75d8c2a
PIero <piero.hierro1@gmail.com>
2021-10-20 15:42:57 +0200

    ADD BUGGEDSOURCECODE FROM XPIEROXK

--- LVL2_T353i/Documenta/BuggedSourceCode.c
+++ LVL2_T353i/Documenta/BuggedSourceCode.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    int arr[] = {1, 2, 3, 4, 5, 6 ,};
+    /*Questo programma prende in input un indice e poi stampa
+     * un indirizzo. L'indirizzo stampato e`: indirizzo base dell'array arr + offset dato dall'indice*/
+    int arr[] = {1, 2, 3, 4, 5, 6};
     printf("Inserisci un indice: \n");
     int numero;
-    scanf("%d");
-    printf(&arr[numero]);
+    scanf("%d",&numero);
+    printf("%p",&arr[numero]);
+    return 0;
 }
