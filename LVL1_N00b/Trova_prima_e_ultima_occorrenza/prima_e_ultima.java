8d9009205ea0e536da4f8740ee35536481f7081c
LujaLDP <jeaxcommunityluja@gmail.com>
2021-10-20 14:15:08 +0200

    prima e ultima occorrenza

---
+++ LVL1_N00b/Trova_prima_e_ultima_occorrenza/prima_e_ultima.java
@@ -0,0 +1,10 @@
+package LVL1_N00b.Trova_prima_e_ultima_occorrenza;
+
+public class prima_e_ultima {
+    public static void main(String[] args) {
+    int[] a = {69,3,5,7,21};
+    int primo = a[0];
+    int ultimo = a[a.length - 1];
+    System.out.println("PogChamp:" + primo + "\n" + "NotLikeThis:" + ultimo);
+    }
+}
