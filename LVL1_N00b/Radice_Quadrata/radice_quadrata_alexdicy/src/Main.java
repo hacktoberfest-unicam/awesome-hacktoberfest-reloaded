6dd34f1e74e7fb7b1e0fcbc8975ca45c68c6ade7
Lorenzo Lapucci <halexercraft@gmail.com>
2021-10-20 12:29:10 +0200

    Add radice quadrata

--- LVL1_N00b/Radice_Quadrata/radice_quadrata_alexdicy/src/Main.java
+++ LVL1_N00b/Radice_Quadrata/radice_quadrata_alexdicy/src/Main.java
@@ -8,13 +8,64 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Inserisci un quadrato perfetto: ");
-        long number = sc.nextLong();
-        double sqrt = Math.sqrt(number);
-        int root = (int) sqrt;
-        if (root != sqrt) {
-            System.out.println("Il quadrato che hai inserito non è perfetto");
+        while (true) {
+            System.out.print("Inserisci un quadrato perfetto: ");
+            int number = sc.nextInt();
+
+            int[] factors = new int[number];
+            int factorsIndex = 0;
+
+            // quante volte è presente il fattore
+            int counter = 0;
+            int prime = nextPrime(1);
+            // finché è possibile dividere
+            while (prime <= number) {
+                // se il numero primo divide il numero
+                if ((double) number / prime % 1 == 0) {
+                    counter++;
+                    // dividi il numero per il fattore primo
+                    number = number / prime;
+                    if (counter % 2 == 0) {
+                        // aggiungi il fattore all'array
+                        factors[factorsIndex++] = prime;
+                    }
+                } else {
+                    // se il fattore è presente un numero dispari di volte il numero non è un quadrato perfetto
+                    if (counter % 2 != 0) {
+                        break;
+                    }
+                    prime = nextPrime(prime);
+                    counter = 0;
+                }
+            }
+
+            if (counter % 2 == 0) {
+                System.out.println("Il quadrato che hai inserito è perfetto");
+                int root = 1;
+                // moltiplica i fattori della radice
+                for (int f : factors) {
+                    if (f == 0) break;
+                    root *= f;
+                }
+                System.out.println("La radice è " + root);
+            } else {
+                System.out.println("Il quadrato non è perfetto");
+            }
         }
-        System.out.println("Risultato intero: " + root);
+    }
+
+    private static boolean isPrime(int number) {
+        if (number <= 3) {
+            return true;
+        }
+        for (int i = number / 2 + 1; i > 1; i--) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static int nextPrime(int value) {
+        while (!isPrime(++value));
+        return value;
     }
 }
