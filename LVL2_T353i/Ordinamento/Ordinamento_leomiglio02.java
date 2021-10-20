553df2bd52ee7a33918a5da0f8cd6410df35c890
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2021-10-20 11:47:55 +0200

    Ordinamento Bubble Sort

--- LVL2_T353i/Ordinamento/Ordinamento_leomiglio02.java
+++ LVL2_T353i/Ordinamento/Ordinamento_leomiglio02.java
@@ -5,13 +5,23 @@ class Ordinamento {
         this.array = array;
     }
 
-    public Order(){
-        for(int i = 0; i < array.length; i++)
-            
+    public void Order() {
+        int temp;
+        for (int i = 0; i < array.length; i++)
+            for (int j = 0; j < array.length; j++)
+                if (array[i] < array[j]) {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+
     }
+
     public static void main(String[] args) {
         int[] array = { 1, 5, 2, 4, 7, 3, 6, 9, 8 };
         Ordinamento o = new Ordinamento(array);
-
+        o.Order();
+        for (int i = 0; i < o.array.length; i++)
+            System.out.print(o.array[i] + " ");
     }
 }
