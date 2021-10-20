7f942f11a9a933a1abb0478bc66baf6e0ba3daba
LujaLDP <jeaxcommunityluja@gmail.com>
2021-10-20 12:38:19 +0200

    trova il duplicato in java

---
+++ LVL1_N00b/Trova_il_Duplicato/trova_il_duplicato.java
@@ -0,0 +1,10 @@
+package LVL1_N00b.Trova_il_Duplicato;
+
+public class trova_il_duplicato {
+    public static void main(String[] args) {
+    int[] a = {69,3,5,7,21};
+    int primo = a[0];
+    int ultimo = a[a.length - 1];
+    System.out.println("PogChamp:" + primo + "\n" + "NotLikeThis:" + ultimo);
+    }
+}
